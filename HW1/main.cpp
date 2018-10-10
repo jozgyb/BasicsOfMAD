d0c2945dd901c17f981b2259a5f37fdc65577b67
jozgyb <jzsab1@gmail.com>
2018-10-10 12:27:27 +0200

    Diamond problem

--- HW1/main.cpp
+++ HW1/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+class SuperAncestor {
+public:
+    void sayHello() {
+        std::cout << "I'm a class." << std::endl;
+    }
+};
+
+class DescendantA : virtual public SuperAncestor {
+
+};
+
+class DescendantB : virtual public SuperAncestor {
+
+};
+
+class MultipleDescendant : public DescendantA, public DescendantB {
+
+};
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+//    Diamond problem! If we remove the virtual keywords at the inheritance part, we will get an error, because MultipleDescendant
+//    derives from DescendantA and DescendantB, which each have there copy of their sayHello() method.
+//    Thats why we make the inheritance virtual, this C++ will ensure that only one subobject of the
+//    SuperAncestor class will be created for every MultipleDescendant Obejct.
+    MultipleDescendant example;
+    example.sayHello();
 }
